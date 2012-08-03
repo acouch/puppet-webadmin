df838d650b3591927371a79498fa79cc61c3f4ed
acouch <acinternets@gmail.com>
2012-08-03 13:06:50 -0500

    removing separate bashrc file

--- manifests/dev.pp
+++ manifests/dev.pp
@@ -10,11 +10,4 @@ class webadmin::dev($webadminuser = $webadmin::webadminuser, $webadmingroup = $w
     source => "puppet:///modules/webadmin/defhost",
   }
 
-  file { "/home/$webadminuser/.bashrc":
-    ensure => present,
-    owner => $webadminuser,
-    group => webadmingroup,
-    mode => 644,
-    source => "puppet:///modules/webadmin/.bashrc",
-  }
 }
