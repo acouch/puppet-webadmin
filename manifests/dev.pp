586a2f99db42f76bb4e7e664db6c1bc4e3b341cd
acouch <acinternets@gmail.com>
2013-03-29 09:05:11 -0500

    Removing defhost

--- manifests/dev.pp
+++ manifests/dev.pp
@@ -1,15 +1,6 @@
 # Dev class for webadmin submodule
 class webadmin::dev($webadminuser = $webadmin::webadminuser, $webadmingroup = $webadmin::webadmingroup) {
 
-  # Add default vhost for simple development. Necessary pre-getSite.
-  file { "/etc/apache2/sites-available/defhost":
-    owner => $webadminuser,
-    group => $webadmingroup,
-    mode => 644,
-    ensure => file,
-    source => "puppet:///modules/webadmin/defhost",
-  }
-
   file { "/home/$webadminuser/.bashrc":
     ensure => present,
     owner => $webadminuser,
